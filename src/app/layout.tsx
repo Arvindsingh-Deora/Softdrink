149df828adb464d457f655554569cf210c60ad41
Arvind Deora <arvindsinghdeora47@gmail.com>
2025-07-19 14:42:48 +0000

    can you change the fabicon into this https://cdn-icons-png.flaticon.com/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="https://cdn-icons-png.flaticon.com/128/15003/15003559.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Belleza&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
